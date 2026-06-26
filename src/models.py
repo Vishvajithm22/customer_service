f788ed3cc22e1ddd673ef4bffa5a050c541c4626
Vishvajithm22 <vishvajithm22@gmail.com>
2026-06-26 13:29:04 +0530

    Add yearly_sale, sector, grade and category fields

--- src/models.py
+++ src/models.py
@@ -1,12 +1,25 @@
 from .database import Base
-from sqlalchemy import Column,Integer,String,Boolean
+from sqlalchemy import Column, Integer, String, Float
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 from sqlalchemy import func
 
+
 class CUSTOMERS(Base):
-    __tablename__="customers"
-    id=Column(Integer,autoincrement=True,primary_key=True)
-    name=Column(String,nullable=False)
-    email=Column(String,nullable=False,unique=True)
-    phone=Column(String,nullable=False)
-    created_at=Column(TIMESTAMP(timezone=True),nullable=False,server_default=func.now())
+    __tablename__ = "customers"
+
+    id = Column(Integer, autoincrement=True, primary_key=True)
+
+    name = Column(String, nullable=False)
+    email = Column(String, nullable=False, unique=True)
+    phone = Column(String, nullable=False)
+
+    yearly_sale = Column(Float, nullable=False)
+    sector = Column(String, nullable=False)
+    grade = Column(String, nullable=False)
+    category = Column(String, nullable=False)
+
+    created_at = Column(
+        TIMESTAMP(timezone=True),
+        nullable=False,
+        server_default=func.now()
+    )
