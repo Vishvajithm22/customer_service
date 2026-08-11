50626c0b7d3aabfc994239f3949cb9da5438f2c2
Vishvajithm22 <vishvajithm22@gmail.com>
2026-08-11 23:26:43 +0530

    Removed the monitoring dashboard

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ import logging
 from src import models
 from fastapi import FastAPI, Depends, HTTPException
 from typing import Annotated
-from prometheus_fastapi_instrumentator import Instrumentator
+
 
 Base.metadata.create_all(bind=engine)
 
@@ -96,7 +96,7 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 app = FastAPI()
-Instrumentator().instrument(app).expose(app)
+
 
 
 
